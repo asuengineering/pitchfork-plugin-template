73b6222771e7ad65243ff896b1ebae1fe99d40c7
Steve Ryan <steve.ryan@asu.edu>
2023-12-01 16:11:15 -0700

    Enqueue styles by default.

--- inc/enqueue-scripts.php
+++ inc/enqueue-scripts.php
@@ -10,13 +10,13 @@
 // Possible hooks: enqueue_block_assets (both), enqueue_block_editor_assets
 // Old school hooks: wp_enqueue_scripts, wp_enqueue_style
 
-add_action( 'enqueue_block_editor_assets', 'starter_plugin_enqueue_block_scripts' );
+add_action( 'enqueue_block_assets', 'starter_plugin_enqueue_block_scripts' );
 function starter_plugin_enqueue_block_scripts() {
 
 		$the_plugin     = get_plugin_data( plugin_dir_path( __DIR__ ) . 'pitchfork-plugin-template.php' );
 		$the_version    = $the_plugin['Version'];
-		$plugin_version = $the_version . '.' . filemtime( plugin_dir_path( __DIR__ ) . 'js/block-variations.js' );
+		$plugin_version = $the_version . '.' . filemtime( plugin_dir_path( __DIR__ ) . '/dist/css/plugin.css' );
 
-	wp_enqueue_script( 'pf-plugin-template-block-variations', plugin_dir_url( __DIR__ ) . 'js/block-variations.js',  array( 'wp-blocks', 'wp-dom' ), $plugin_version , true );
+	wp_enqueue_style( 'pitchfork-plugin-template-style', plugin_dir_url( __DIR__ ) . '/dist/css/plugin.css', array(), $plugin_version);
 
 }
